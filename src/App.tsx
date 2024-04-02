19f1c66083b43ad5fa81553816a07cb7915ddd04
Sara <sara.maldum@husbanken.no>
2024-04-02 20:23:12 +0200

    feat: legge til bilforsikring og endre i App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import BilforsikringSkjema from './skjema/bilforsikring-skjema';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Kjøp Bilforsikring</h1>
+        <p className="ingress">Det er fire forskjellige forsikringer å velge mellom. Ansvarsforsikring er lovpålagt om kjøretøyet er
+        registrert og skal brukes på veien. I tillegg kan du utvide forsikringen avhengig av hvor gammel bilen din
+        er og hvordan du bruker den.</p>
       </header>
+        <main>
+            <BilforsikringSkjema></BilforsikringSkjema>
+        </main>
     </div>
   );
 }
